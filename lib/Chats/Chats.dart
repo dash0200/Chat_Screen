9e34b7c75765c7f8dabdb58c6c45dc494b85a67c
dash0200 <darshanvanakudre@gmail.com>
2021-05-25 11:40:17 +0530

    Updated ListView

--- lib/Chats/Chats.dart
+++ lib/Chats/Chats.dart
@@ -1,28 +1,3 @@
-// final List<String> link = [
-//   'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-//   'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-//   'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
-//   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-Pgu0K3ElJI72V0zt7N81CvfljwsJr_JcV_9JvQvvADA-5loy0FrIvCH4DBRffbf4w38&usqp=CAU',
-//   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBECMLIhdR3CKNQUpJY49mcTqy9XtxxK61Ow&usqp=CAU',
-//   'https://expertphotography.com/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg',
-// ];
-// final List<String> name = [
-//   'James Ricky',
-//   'Daniel Radic',
-//   'Robert S',
-//   'Alex, GG',
-//   'Ron W',
-//   'Magnum Trex',
-// ];
-// final List<String> msg = [
-//   'Hi How are you',
-//   'Idk know if it\'s true butthey said its officialnot sure how much of',
-//   'Hi How are you',
-//   'Hi How are you',
-//   'Hi How are you',
-//   'Hi How are you',
-// ];
-
 class Message {
   late final String name;
   late final String link;
